9dbf4bc583f53bbee6cca34bede6f20f702db321
Raulloco <raul-luar@hotmail.com.br>
2024-05-19 22:34:22 -0300

    Sistema de Dialogo
    só pq sim

---
+++ AnoesECavernas/Assets/Dialog.cs
@@ -0,0 +1,178 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialog : MonoBehaviour
+{
+    //vai ser usado para comparar os nomes(para ver se são parte do mesmo dialogo)
+    private string cName=" ";
+
+    //Os dialogos
+    public Dialogs[] AllDialogs;
+    //usado pra armazenar todos os gameobject
+    private GameObject[] AllDialogsAux;
+    //array que indica se o gameobject em AllDialogsAux é dialog (1) ou subdialogo (0) ///poderia ter sido booleano, mas caso exista mais de um estado possivel deixei em int
+    private int[] BinaryDialogStart;
+    //quantidade de dialogos diferentes(já considerando os subdialogos)
+    private int dialogcount;
+
+    //O struct dos dialogos
+    [System.Serializable]
+    public struct Dialogs
+    {
+        //[HideInInspector]
+        public string Name;
+
+        public GameObject[] gameObj;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        //nmr total de dialogos e subdialogos
+        int childCount=transform.GetChild(0).gameObject.transform.childCount;
+        BinaryDialogStart = new int[childCount];
+        AllDialogsAux= new GameObject[childCount];
+
+        for (int i=0; i<childCount; i++) 
+        {
+            
+            //pega os gameobject
+            AllDialogsAux[i] = transform.GetChild(0).gameObject.transform.GetChild(i).gameObject;
+
+            if(i==0)
+            { 
+                //Torna cName o nome do primeiro dialogo(deve ser obrigatoriamente um dialogo e n subdialogo)
+                cName = AllDialogsAux[i].name;
+                BinaryDialogStart[i] = 1;
+                dialogcount++;
+            }
+            else if (cName== AllDialogsAux[i].name.Substring(0, cName.Length)) //compara se o nome é o mesmo
+            {
+                BinaryDialogStart[i] = 0;
+            }
+            else 
+            {
+                //Muda o cName para o nome do proximo dialogo
+                cName = AllDialogsAux[i].name;
+                BinaryDialogStart[i] = 1;
+                dialogcount++;
+            }
+        }
+
+        //usado para identificar qual subdialogo está
+        int subdialogtracker = 1;
+        //usado para identificar qual dialogo está
+        int dialogtracker = 0;
+
+        //usado para indentificar quantos subdialogos tem cada dialogo (+1 para o dialogo em si)
+        int[] subdialogmatrix= new int[dialogcount];
+
+        //faz a cotagem dos subdialogos e armazena na matrix
+        for(int i=1; i<childCount; i++) 
+        {
+            
+            if (BinaryDialogStart[i] == 1)
+            {
+                subdialogmatrix[dialogtracker] = subdialogtracker;
+                dialogtracker++;
+                subdialogtracker = 1;
+            }
+            else 
+            {
+                subdialogtracker++;
+            }
+        }
+        //o for só armazena o valor quado chega no próximo dialogo, aqui o ultimo dialogo é armazenado
+        subdialogmatrix[dialogtracker] = subdialogtracker;
+
+        //inicializa a matriz dos dialogos
+        AllDialogs = new Dialogs[dialogcount];
+
+        //reinicia os trackers
+        dialogtracker = 0;
+        subdialogtracker = 0;
+
+        //inicializa a matriz de gameobjects dentro do primeiro dialogo
+        AllDialogs[0].gameObj = new GameObject[subdialogmatrix[0]];
+        //armazena o primeiro gameobject de dialogo
+        AllDialogs[0].gameObj[0] = AllDialogsAux[0];
+        //armazena o nome do dialogo
+        AllDialogs[0].Name = AllDialogs[0].gameObj[0].name;
+
+
+
+        for (int i=1; i<childCount; i++) 
+        { 
+            if (BinaryDialogStart[i]==1)
+            {
+                //proximo dialogo
+                dialogtracker++;
+                //inicializa a matriz de gameobjects dentro do atual dialogo
+                AllDialogs[dialogtracker].gameObj = new GameObject[subdialogmatrix[dialogtracker] ];
+                AllDialogs[dialogtracker].gameObj[0] = AllDialogsAux[i];
+                AllDialogs[dialogtracker].Name = AllDialogs[dialogtracker].gameObj[0].name;
+                //reinicia a contagem de subdialogos
+                subdialogtracker = 0;
+            }
+            else
+            {
+                subdialogtracker++;
+                //armazena os subdialogos
+                AllDialogs[dialogtracker].gameObj[subdialogtracker] = AllDialogsAux[i];
+            }
+        }
+    }
+
+    public int TestDialogId,Conversa;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E)) {DialogStart(TestDialogId);}
+        if (Input.GetKeyDown(KeyCode.Alpha1)) { TestDialogId = 0; }
+        if (Input.GetKeyDown(KeyCode.Alpha2)) { TestDialogId = 1; }
+        if (Input.GetKeyDown(KeyCode.Alpha3)) { TestDialogId = 2; }
+        if (Input.GetKeyDown(KeyCode.Space)) { DialogEnd(Conversa); }
+    }
+
+    void DialogStart(int DialogId) 
+    {
+        if (Conversa == -1)
+        {
+            transform.GetChild(0).gameObject.SetActive(true);
+            AllDialogs[DialogId].gameObj[0].SetActive(true);
+            Conversa = DialogId;
+        }
+    }
+    void DialogEnd(int DialogId)
+    {
+        if (DialogId != -1)
+        {
+            int DialogSize = AllDialogs[DialogId].gameObj.Length;
+            int Ativo = -1;
+            for (int i = 0; i < AllDialogs[DialogId].gameObj.Length; i++)
+            {
+                if (AllDialogs[DialogId].gameObj[i].activeInHierarchy)
+                {
+                    Ativo = i;
+                    break;
+                }
+            }
+            if (Ativo != -1)
+            {
+                AllDialogs[DialogId].gameObj[Ativo].SetActive(false);
+
+                if (DialogSize > Ativo + 1)
+                {
+                    AllDialogs[DialogId].gameObj[Ativo + 1].SetActive(true);
+                }
+                else
+                {
+                    transform.GetChild(0).gameObject.SetActive(false);
+                    Conversa = -1;
+                }
+            }
+        }
+    }
+    
+}
