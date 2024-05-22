089ecb3b723e945b07a9f600e42de0e412e735f1
Raulloco <raul-luar@hotmail.com.br>
2024-05-22 02:08:50 -0300

    MinorChange
    Adição de um offset baseado em tiles

--- AnoesECavernas/Assets/Cursor.cs
+++ AnoesECavernas/Assets/Cursor.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
-
 public class Cursor : MonoBehaviour
 {
     private Grid grid;
@@ -11,11 +10,13 @@ public class Cursor : MonoBehaviour
     private Color color;
     public Color NewColor;
     public Vector3 offset;
+    public Vector3Int offsetTilezed;
     // Start is called before the first frame update
     void Start()
     {
         //seleciona o grid
-        grid = GameObject.FindGameObjectWithTag("Tile").GetComponent<Grid>();
+        if(GameObject.FindGameObjectWithTag("Tile").activeInHierarchy) grid = GameObject.FindGameObjectWithTag("Tile").GetComponent<Grid>();
+        else { grid = null; Debug.Log("Usando o tilemap errado"); }
         color = Sprit.GetComponent<SpriteRenderer>().color;
     }
 
@@ -32,7 +33,7 @@ public class Cursor : MonoBehaviour
         transform.position =grid.CellToWorld(grid.WorldToCell(rmouseposition));
 
         //restaura o valor de z e aplica offset
-        transform.position = offset + new Vector3(transform.position.x,transform.position.y,z);
+        transform.position = offset + new Vector3(transform.position.x,transform.position.y,z) + new Vector3(offsetTilezed.x*grid.cellSize.x, offsetTilezed.y * grid.cellSize.y, 0);
 
         
         if (caminho.HasTile(grid.WorldToCell(rmouseposition)) && caminho.GetTile(grid.WorldToCell(rmouseposition))!=null) 
